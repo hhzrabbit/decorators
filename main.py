33cd5a3e8f1a3bc3e5c7f96fdb58f0d50ab080d9
Haley Zeng <hzeng@stuy.edu>
2017-04-30 23:31:42 -0400

    done

--- main.py
+++ main.py
@@ -1,10 +1,67 @@
-def wrapper( f ):
+import time
+
+def name_arg( f ):
    def inner( *arg ):
-       return f( *arg )
+      print "%s: %s" % (f.func_name, str(arg))
+      return f( *arg )
    return inner
 
-def foo(a, b, c):
-   return str(a * b) + c
+def timer( f ):
+   def inner( *arg ):
+      start = time.time()
+      f( *arg )
+      end = time.time()
+      return "execution time: %s" % (end - start)
+   return inner
+   
+@name_arg
+def fib(n):
+    if n < 2:
+        return n
+    return fib(n-2) + fib(n-1)
 
-closure = wrapper(foo)
-print closure( -2, 3, 'hello' )
+@timer
+def fib_timed(n):
+    if n < 2:
+        return n
+    return fib(n-2) + fib(n-1)
+ 
+print "Fibonacci"
+print "--"
+print fib(4)
+print fib_timed(4)
+print "--"
+print fib(5)
+print fib_timed(5)
+print "--"
+print fib(6)
+print fib_timed(6)
+print "--"
+print
+
+@name_arg
+def fact(n):
+   if n == 1:
+      return n
+   return n * fact(n-1)
+
+@timer
+def fact_timed(n):
+   if n == 1:
+      return n
+   return n * fact(n-1)
+
+print "Factorial"
+print "--"
+print fact(3)
+print fact_timed(3)
+print "--"
+print fact(4)
+print fact_timed(4)
+print "--"
+print fact(5)
+print fact_timed(5)
+print "--"
+print
+
+
